5b797dd91eeeb3f42e6b69b7de8a13a40eea90de
Chris Twiner <chris.twiner@gmail.com>
2024-02-27 12:05:49 +0100

    #1 #2 - 14.0 support and docs

---
+++ runtime/src/main/14.0.dbr-scala/com/sparkutils/shim/expressions/Compat.scala
@@ -0,0 +1,25 @@
+package com.sparkutils.shim.expressions
+
+import org.apache.spark.sql.catalyst.expressions.{Expression, HigherOrderFunction, Nondeterministic, LambdaFunction => SLambdaFunction, Unevaluable}
+import org.apache.spark.sql.types.DataType
+
+trait StatefulLike extends Nondeterministic {
+  /**
+   * Return a fresh uninitialized copy of the stateful expression.
+   */
+  def freshCopy(): StatefulLike
+
+}
+
+trait HigherOrderFunctionLike extends HigherOrderFunction {
+  override def bind(f: (Expression, Seq[(DataType, Boolean)]) => SLambdaFunction): HigherOrderFunction =
+    bindInternal(f)
+
+  protected def bindInternal(f: (Expression, Seq[(DataType, Boolean)]) => SLambdaFunction): HigherOrderFunction
+}
+
+/**
+ * 2.4 and 3.0 version doesn't have foldable as false so the optimiser tries to fold, we need Unevaluable for 14.4
+ */
+trait FoldableUnevaluable extends Unevaluable {
+}
