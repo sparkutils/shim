d840ebd278113bad81dbaadae6873f0bddcf03d7
Chris Twiner <chris.twiner@gmail.com>
2024-03-07 17:50:54 +0100

    #2 - add future compat on deprecated forwarding functions, still present in 4 as of 07 03 at least ~cut-release

--- runtime/src/main/scala/com/sparkutils/shim/expressions/functions.scala
+++ runtime/src/main/scala/com/sparkutils/shim/expressions/functions.scala
@@ -1,6 +1,10 @@
 package com.sparkutils.shim.expressions
 
-import org.apache.spark.sql.catalyst.expressions.{Coalesce, Expression}
+import org.apache.spark.sql
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.catalyst.expressions.{Coalesce, CreateNamedStruct, CreateStruct, Expression}
+
+import scala.annotation.nowarn
 
 /*
  * Collection of shims on really stable interfaces for functional expressions
@@ -12,4 +16,30 @@ object Coalesce1 {
 
   def unapply(coalesce: Coalesce): Option[scala.Seq[Expression]] =
     Coalesce.unapply(coalesce)
+}
+
+// frameless has 5 deprecated functions which are now errors, but for source compat they need to stay, no idea as to when they will
+// be removed, shim later..
+object functions {
+
+  @nowarn
+  def sumDistinct(e: Column): Column = sql.functions.sumDistinct(e)
+
+  @nowarn
+  def shiftRightUnsigned(e: Column, numBits: Int): Column = sql.functions.shiftRightUnsigned(e, numBits)
+
+  @nowarn
+  def shiftRight(e: Column, numBits: Int): Column = sql.functions.shiftRight(e, numBits)
+
+  @nowarn
+  def shiftLeft(e: Column, numBits: Int): Column = sql.functions.shiftLeft(e, numBits)
+
+  @nowarn
+  def bitwiseNOT(e: Column): Column = sql.functions.bitwiseNOT(e)
+
+}
+
+object CreateStruct1 {
+  def apply(children: scala.Seq[Expression]): CreateNamedStruct =
+    CreateStruct.apply(children)
 }
