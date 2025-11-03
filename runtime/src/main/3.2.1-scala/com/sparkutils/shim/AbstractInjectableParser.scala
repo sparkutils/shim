483d7abc12d60c7c75dd5e7cf636ee3051035cc0
Chris Twiner <chris.twiner@gmail.com>
2025-11-03 21:33:40 +0100

    #18 - add abstract parser shim for quality lambda connect support ~cut-release

---
+++ runtime/src/main/3.2.1-scala/com/sparkutils/shim/AbstractInjectableParser.scala
@@ -0,0 +1,33 @@
+package com.sparkutils.shim
+
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.expressions.Expression
+import org.apache.spark.sql.catalyst.{FunctionIdentifier, TableIdentifier}
+import org.apache.spark.sql.catalyst.parser.ParserInterface
+import org.apache.spark.sql.catalyst.plans.logical.{LeafCommand, LogicalPlan}
+import org.apache.spark.sql.types.{DataType, StructType}
+
+/**
+ * Simple no op
+ * @param command for logging only
+ * @param parameters for logging only
+ */
+case class NoOpCommand(command: String, parameters: Seq[String]) extends LeafCommand
+
+class AbstractInjectableParser(sparkSession: SparkSession, delegate: ParserInterface) extends ParserInterface with Logging {
+
+  override def parsePlan(sqlText: String): LogicalPlan = delegate.parsePlan(sqlText)
+
+  override def parseExpression(sqlText: String): Expression = delegate.parseExpression(sqlText)
+
+  override def parseTableIdentifier(sqlText: String): TableIdentifier = delegate.parseTableIdentifier(sqlText)
+
+  override def parseFunctionIdentifier(sqlText: String): FunctionIdentifier = delegate.parseFunctionIdentifier(sqlText)
+
+  override def parseMultipartIdentifier(sqlText: String): Seq[String] = delegate.parseMultipartIdentifier(sqlText)
+
+  override def parseTableSchema(sqlText: String): StructType = delegate.parseTableSchema(sqlText)
+
+  override def parseDataType(sqlText: String): DataType = delegate.parseDataType(sqlText)
+}
